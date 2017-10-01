1fc753ac090b9002058154680043186c19ae7c39
Anthony <afeliciano893@gmail.com>
2017-09-30 21:18:08 -0400

    with comments

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -15,7 +15,7 @@ export class UserComponent  {
 	showHobbies: boolean;
 	posts:Post[];
 
-
+	// makes sense of what was entered above? name, email, address??
 	constructor(private postsService: PostsService) {
 		this.name = 'John Doe';
 		this.email = 'jon@gmail.com',
@@ -27,13 +27,13 @@ export class UserComponent  {
 		this.hobbies = ['music', 'movies','sports'];
 		this.showHobbies=false;
 
+		//getPosts defined in ./services/post.service.ts
 		this.postsService.getPosts().subscribe(posts=> {
 			this.posts = posts;
 		});
-
-
 	}
 
+	//if the button is clicked show hidehobbies, else dont 
 	toggleHobbies() {
 		if(this.showHobbies==true){  
 			this.showHobbies=false;
@@ -41,15 +41,18 @@ export class UserComponent  {
 			this.showHobbies=true;
 		}
 	}
+	//append single hobby object to a hobbies list defined above 
 	addHobby(hobby) {
 		this.hobbies.push(hobby);
 	}
-
+	//make sure i=index in ngFor in html 
 	deleteHobby(i) {
 		this.hobbies.splice(i, 1);
 	}
 }
 
+
+// create methods to unpack object that contains multiple elements?
 interface address {
 	street: string;
 	city: string;
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -2,12 +2,14 @@ import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
+//use inject decorator (Injectable) give added functionality (http capabilities) to a specific component (PostsService)
 @Injectable()
 export class PostsService {
 	constructor(private http: Http){
-		console.log('posts service initt....')
+		console.log('Posts Service init....')
 	}
 
+	//maps response from http request to json parser?
 	getPosts(){
 		return this.http.get('https://jsonplaceholder.typicode.com/posts')
 			.map(res => res.json());
